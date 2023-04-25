e60af70a918d45f943bff0f98f988c1154bbcecd
Jiaoyang Li <jiaoyang.li_@outlook.com>
2023-04-25 11:57:44 -0500

    correct a typo! come on Jiaoyang

--- library_generation/grid/xml/submitgrid.sh
+++ library_generation/grid/xml/submitgrid.sh
@@ -20,20 +20,20 @@ project.py --xml ./argon_xml/prod_optical_dune_argon05.xml --stage gen --clean
 project.py --xml ./argon_xml/prod_optical_dune_argon05.xml --stage gen --submit
 
 # xenon
-project.py --xml ./xenon_xml/prod_optical_dune_argon00.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon00.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon00.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon00.xml --stage gen --submit
 
-project.py --xml ./xenon_xml/prod_optical_dune_argon01.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon01.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon01.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon01.xml --stage gen --submit
 
-project.py --xml ./xenon_xml/prod_optical_dune_argon02.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon02.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon02.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon02.xml --stage gen --submit
 
-project.py --xml ./xenon_xml/prod_optical_dune_argon03.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon03.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon03.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon03.xml --stage gen --submit
 
-project.py --xml ./xenon_xml/prod_optical_dune_argon04.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon04.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon04.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon04.xml --stage gen --submit
 
-project.py --xml ./xenon_xml/prod_optical_dune_argon05.xml --stage gen --clean
-project.py --xml ./xenon_xml/prod_optical_dune_argon05.xml --stage gen --submit
+project.py --xml ./xenon_xml/prod_optical_dune_xenon05.xml --stage gen --clean
+project.py --xml ./xenon_xml/prod_optical_dune_xenon05.xml --stage gen --submit
