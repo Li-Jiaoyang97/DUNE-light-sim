ccd8ce7875eb6bd3d355cffcb7d5082c1c6543d9
Jiaoyang Li <jiaoyang.li_@outlook.com>
2023-06-05 16:05:22 -0500

    the semi-final version of light simulation sample generation work-flow

--- library_generation/lib_analyse/lib_grid_output_processing.cc
+++ library_generation/lib_analyse/lib_grid_output_processing.cc
@@ -1,21 +1,20 @@
 #include "TChain.h"
 #include "TFile.h"
 #include "TTree.h"
+#include "TSystem.h"
 #include <iostream>
 #include <fstream>
 
-void lib_grid_output_processing();
-TChain * CreateChainFromList_opt(std::string ListFileName, std::string ChainName, bool DoCheck=false);
+TChain * CreateChainFromList_opt(std::string ListFileName, std::string ChainName, bool DoCheck, TString zombine_files);
 
-
-void lib_grid_output_processing()
+void lib_grid_output_processing(TString type = "argon", TString zombine_files = "./List_zombies_argon")
 { 
   bool debug = false;
   std::cout<<"Begining AssembleSingleFile.C\n";
   
-  std::string type = "xenon_new"; //xenon argon
-  std::string FileList = "./List_of_Grid_files_"+type; //List_of_Grid_files
-  TChain * ch = CreateChainFromList_opt(FileList.c_str(),"pmtresponse/PhotonLibraryData",false);
+  std::string FileList = "./List_of_Grid_files_"+std::string(type); //List_of_Grid_files
+  
+  TChain * ch = CreateChainFromList_opt(FileList.c_str(), "pmtresponse/PhotonLibraryData", true, zombine_files);
 
   Int_t Voxel, OpChannel;
   Float_t Visibility;
@@ -25,9 +24,12 @@ void lib_grid_output_processing()
   
   std::cout<<"Create Output File\n";
 
-  const TString root_file_name = "Photon_library_dunevd10kt_3view_v2_refactored_1x8x14ref_ReflAnode_"+type+".root";
+  TString save_dir = "/dune/data/users/jiaoyang/newLib0503/";
+  gSystem->Exec("mkdir -p " + save_dir);
+  //gSystem->Exec("rm "+save_dir+"*");
+  const TString root_file_name = save_dir+"dunevd10kt_3view_30deg_v5_refactored_1x8x14ref_"+type+"_new.root";
   TFile *f = new TFile(root_file_name,"RECREATE");
-  TTree * tt = new TTree("PhotonLibraryData","PhotonLibraryData");
+  TTree *tt = new TTree("PhotonLibraryData","PhotonLibraryData");
   tt->Branch("Voxel",      &Voxel,      "Voxel/I");
   tt->Branch("OpChannel",  &OpChannel,  "OpChannel/I");
   tt->Branch("Visibility", &Visibility, "Visibility/F");
@@ -71,36 +73,38 @@ void lib_grid_output_processing()
   f->Close();
 }
 
-TChain * CreateChainFromList_opt(std::string ListFileName, std::string ChainName, bool DoCheck)
+TChain * CreateChainFromList_opt(std::string ListFileName, std::string ChainName, bool DoCheck, TString zombine_files)
 {
   ifstream InputFile(ListFileName.c_str());
   std::string FileName;
 
   TChain * TheChain = new TChain(ChainName.c_str());
-  if(!DoCheck)
-    {
-      while(getline(InputFile, FileName))
-        {
-          //FileName=BaseDirectory+FileName;
-          std::cout<<"Adding file "<< FileName.c_str()<<" to the TChain"<<std::endl;
+  if(!DoCheck){
+    while(getline(InputFile, FileName)){
+      //FileName=BaseDirectory+FileName;
+      std::cout<<"Adding file "<< FileName.c_str()<<" to the TChain"<<std::endl;
+      TheChain->Add(FileName.c_str());
+    }
+  }
+  else{
+    std::ofstream outputFile(zombine_files);
+    while(getline(InputFile, FileName)){
+      //FileName=BaseDirectory+FileName;
+      TFile *infile = TFile::Open(FileName.c_str());
+      if (infile && !infile->IsZombie()){
+        //if(infile->Get(ChainName.c_str())){
+        //std::cout<<"Adding file "<< FileName.c_str()<<" to the TChain"<<std::endl;
+        //if (TheChain->GetFile() && !TheChain->GetFile()->IsZombie()){
           TheChain->Add(FileName.c_str());
+        }else{
+          //std::cout << "Zombie file detected: " << filename << std::endl;
+          outputFile << FileName.c_str() << std::endl;
         }
+      /*}else {
+        outputFile << FileName.c_str() << std::endl;
+      }*/
     }
-  else
-    {
-      while(getline(InputFile, FileName))
-        {
-          //FileName=BaseDirectory+FileName;
-          TFile*f=TFile::Open(FileName.c_str());
-          if(f->Get(ChainName.c_str()))
-            {
-              TheChain->Add(FileName.c_str());
-              std::cout<<"Adding file "<< FileName.c_str()<<" to the TChain"<<std::endl;
-            }
-          else std::cout<<"Chain " <<ChainName << " not found in file " << FileName<<std::endl;
-        }
-
-
-    }
+    outputFile.close();
+  }
   return TheChain;
 }
--- semi_analytical_gen/merge/creat_list.sh
+++ semi_analytical_gen/merge/creat_list.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 #
-find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/argon/semi_analytic_hits_vd_cathode_full_refl_argon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_argon_cathode
+# Author: Jiaoyang Li (jiaoyang.li@ed.ac.uk)
+
+#find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/argon/semi_analytic_hits_vd_cathode_full_refl_argon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_argon_cathode
 find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/argon/semi_analytic_hits_vd_laterals_full_refl_argon_varying_x/out/*/*.root | pnfsToXRootD > List_of_Grid_files_argon_lateral
 find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/argon/semi_analytic_hits_vd_laterals_full_refl_argon_varying_z/out/*/*.root | pnfsToXRootD >> List_of_Grid_files_argon_lateral
 
-find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_cathdoe_full_refl_xenon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_cathode_full_ref
-find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_cathode_without_anode_refl_xenon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_cathode_without_annode_ref
+#find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_cathdoe_full_refl_xenon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_cathode_full_ref
+#find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_cathode_without_anode_refl_xenon/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_cathode_without_annode_ref
 find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_laterals_full_refl_xenon_varying_x/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_lateral_full_ref
 find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_laterals_full_refl_xenon_varying_z/out/*/*.root | pnfsToXRootD >> List_of_Grid_files_xenon_lateral_full_ref
 find /pnfs/dune/scratch/users/jiaoyang/light_generation_dunevd/xenon/semi_analytic_hits_vd_laterals_without_anode_refl_xenon_varing_x/out/*/*.root | pnfsToXRootD > List_of_Grid_files_xenon_lateral_without_annode_ref
